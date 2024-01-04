aed810c49361ecc180acc9d19b67371e78c87bb2
ddaeunbb <ddaeunbb@gmail.com>
2024-01-04 13:57:19 +0900

    [feat]: video 부분 구조 - main 브렌치 업데이트를 위해 커밋을 날립니다.

--- src/pages/newsVideo/NewsVideo.tsx
+++ src/pages/newsVideo/NewsVideo.tsx
@@ -1,7 +1,3 @@
-import BamsaneungBoy from '@/assets/credit/bamsaneung/bamsaneung-boy.svg?react';
-import BigSpeechBalloon from '@/assets/credit/bamsaneung/bamsaneung-speechballoon-big.svg?react';
-import SmallSpeechBalloon from '@/assets/credit/bamsaneung/bamsaneung-speechballoon-small.svg?react';
-import Horizon from '@/components/horizon/Horizon';
 import Navbar from '@/components/navbar/Navbar';
 import { PATH, PATH_NAME } from '@/routers/path';
 
@@ -13,34 +9,20 @@ export function Component() {
         tabNameArr={[PATH_NAME.story, PATH_NAME.video]}
       />
 
-      <div className="pt-8 w-screen flex justify-center max-sm:flex-col">
-        <BamsaneungBoy className="w-44 max-sm:hidden" />
-        <div className="hidden relative max-sm:block mx-auto">
-          <SmallSpeechBalloon className="w-52" />
-        </div>
-
-        <div className="relative">
-          <BigSpeechBalloon className="w-52 max-sm:hidden" />
-        </div>
-        <BamsaneungBoy className="hidden w-44 max-sm:block max-sm:mx-auto max-sm:mt-5" />
-      </div>
-
-      <Horizon />
-
-      <ul className="mt-14 w-10/12 grid bg-lime-300 grid-cols-3 gap-x-10 mx-auto max-lg:grid-cols-2 max-md:grid-cols-1 max-md:gap-y-10">
-        <li className="w-full h-64 bg-blue-300">
+      <ul className="mt-14 max-w-[60rem] max-md:w-11/12 max-sm:w-10/12 mx-auto grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
+        <li className="col-span-1 h-64 bg-blue-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eligendi
           similique harum esse voluptatibus deserunt suscipit, id fugit qui
           voluptatem enim rem nesciunt maxime nihil totam consequatur repellat
           ratione perferendis!
         </li>
-        <li className="w-full h-64 bg-blue-300">
+        <li className="col-span-1 h-64 bg-blue-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eligendi
           similique harum esse voluptatibus deserunt suscipit, id fugit qui
           voluptatem enim rem nesciunt maxime nihil totam consequatur repellat
           ratione perferendis!
         </li>
-        <li className="w-full h-64 bg-blue-300">
+        <li className="col-span-1 h-64 bg-blue-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eligendi
           similique harum esse voluptatibus deserunt suscipit, id fugit qui
           voluptatem enim rem nesciunt maxime nihil totam consequatur repellat
