1c75c3927b73dafb47c0f15dde0633f903d8e284
ddaeunbb <ddaeunbb@gmail.com>
2024-01-24 01:31:14 +0900

    [refactor]: bundle size down - rollup-plugin-visualizer를 추가해 번들사이즈를 줄여주었습니다.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), visualizer({ open: true })],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
