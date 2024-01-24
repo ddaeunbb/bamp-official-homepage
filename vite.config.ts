04d45757330255e0881d1737c5e179fe99027769
ddaeunbb <ddaeunbb@gmail.com>
2024-01-24 11:08:51 +0900

    [refactor]: 이미지압축

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({ open: true })],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 3,
+      },
+      webp: { quality: 75, method: 6 },
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
