17e42871681d011cebd4a923229dd2970ff50b64
ddaeunbb <ddaeunbb@gmail.com>
2025-02-24 12:41:52 +0900

    지원링크수정

--- src/constants/link.ts
+++ src/constants/link.ts
@@ -4,7 +4,7 @@ export const BAMP_LINK = {
   youtube: 'https://www.youtube.com/@bamp3754',
   cafe: 'https://cafe.naver.com/gobamp',
   blog: 'https://blog.naver.com/12bamp',
-  apply: 'https://blog.naver.com/12bamp/223535922435',
+  apply: 'https://blog.naver.com/12bamp/223746760124',
 } as const;
 
 export const YOUTUBE_LINK = {
